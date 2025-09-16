32d750f13b706cb9d2938688e37f3ba1b51ed3ae
姜春波 <jiangchunbo@tq-edu.com>
2025-09-17 02:11:00 +0800

    :pencil: 增加注释，提升代码可读性

--- src/main/java/org/apache/ibatis/cache/CacheKey.java
+++ src/main/java/org/apache/ibatis/cache/CacheKey.java
@@ -23,12 +23,17 @@ import java.util.StringJoiner;
 import org.apache.ibatis.reflection.ArrayUtil;
 
 /**
+ * MyBatis 内部使用的缓存键对象，比较简单，其实将一组对象放在一起，算出一个键
+ *
  * @author Clinton Begin
  */
 public class CacheKey implements Cloneable, Serializable {
 
   private static final long serialVersionUID = 1146682552656046210L;
 
+  /**
+   * 给其他类使用，表示一个空的缓存键，省的创建太多 new CacheKey()
+   */
   public static final CacheKey NULL_CACHE_KEY = new CacheKey() {
 
     private static final long serialVersionUID = 1L;
@@ -45,12 +50,17 @@ public class CacheKey implements Cloneable, Serializable {
   };
 
   private static final int DEFAULT_MULTIPLIER = 37;
+
   private static final int DEFAULT_HASHCODE = 17;
 
   private final int multiplier;
+
   private int hashcode;
+
   private long checksum;
+
   private int count;
+
   // 8/21/2017 - Sonarlint flags this as needing to be marked transient. While true if content is not serializable, this
   // is not always true and thus should not be marked transient.
   private List<Object> updateList;
@@ -67,22 +77,43 @@ public class CacheKey implements Cloneable, Serializable {
     updateAll(objects);
   }
 
+  /**
+   * 返回扰动次数
+   *
+   * @return 扰动次数
+   */
   public int getUpdateCount() {
     return updateList.size();
   }
 
+  /**
+   * 调用一次 update 方法就会扰动内部的值
+   *
+   * @param object 传入对象
+   */
   public void update(Object object) {
+    // null 视作 1
+
     int baseHashCode = object == null ? 1 : ArrayUtil.hashCode(object);
 
     count++;
+
+    // 校验和，所有对象的 hash code 之和
     checksum += baseHashCode;
     baseHashCode *= count;
 
+    // 计算 hash 值
     hashcode = multiplier * hashcode + baseHashCode;
 
+    // 记录有哪些对象参与了 update
     updateList.add(object);
   }
 
+  /**
+   * 传入多个对象扰动
+   *
+   * @param objects 多个对象
+   */
   public void updateAll(Object[] objects) {
     for (Object o : objects) {
       update(o);
@@ -91,19 +122,26 @@ public class CacheKey implements Cloneable, Serializable {
 
   @Override
   public boolean equals(Object object) {
+    // 比较经典的 equals 范式
+
+    // 快速判断是否是同一个对象
     if (this == object) {
       return true;
     }
+
+    // 比较类型
     if (!(object instanceof CacheKey)) {
       return false;
     }
 
     final CacheKey cacheKey = (CacheKey) object;
 
+    // 检查每个字段
     if ((hashcode != cacheKey.hashcode) || (checksum != cacheKey.checksum) || (count != cacheKey.count)) {
       return false;
     }
 
+    // 如果字段是 List 之类的，需要使用 equals 比较每个值
     for (int i = 0; i < updateList.size(); i++) {
       Object thisObject = updateList.get(i);
       Object thatObject = cacheKey.updateList.get(i);
